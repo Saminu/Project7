ecbe42ac066d9173ec724f57110f31371db52e67
Sam <s.salisu@live.co.uk>
2014-12-19 05:09:57 +0000

    WIP

--- www/js/app.js
+++ www/js/app.js
@@ -5,8 +5,27 @@ var Application = angular.module ('Application',[]);
 
 Application.controller('casinoCtrl', ['$scope','$http', function($scope, $http){
 
+    var shuffleArray = function(array) {
+        var m = array.length, t, i;
+
+        // While there remain elements to shuffle
+        while (m) {
+            // Pick a remaining element…
+            i = Math.floor(Math.random() * m--);
+
+            // And swap it with the current element.
+            t = array[m];
+            array[m] = array[i];
+            array[i] = t;
+        }
+
+        return array;
+    }
+
     $http.get('js/listings/casino.json').success(function(data){
         $scope.casino = data;
+
+        shuffleArray($scope.casino);
     });
 
 }]);
